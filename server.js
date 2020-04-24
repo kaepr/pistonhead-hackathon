d07d5453d5f4c9aff06f712a435fccda4d1c9f1b
standmarsh <agrawalshagun07@gmail.com>
2020-04-24 10:22:52 +0530

    yes

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
+const app = express();
+
+//Bodyparser middleware
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+app.use(bodyParser.json());
+
+//DB Config
+const db = require("./config/keys").mongoURI;
+
+//Connect to MongoDB
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        });
+        console.log("MongoDB Conected")
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+connectDB();
+
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport config
+require("./config/passport")(passport);
+
+//routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server up and running on port ${port}`));
+
+/*
+
+mongoose
+    .connect(
+        db,
+        {useNewUrlParser: true, useUnifiedTopology:true}
+    )
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
+
+*/
+
---
+++ validation/login.js
@@ -0,0 +1,28 @@
+const validator = require("validator");
+const isEmpty = require("is-empty");
+
+module.exports = function validateLoginInput(data){
+    let errors = {};
+
+    //convert empty fields to an empty strings so validator can be used
+    data.email = !isEmpty(data.email) ? data.email : "";
+    data.password = !isEmpty(data.password) ? data.password : "";
+
+
+    //Email checks
+    if(validator.isEmpty(data.email)){
+        errors.email = "Email field is required";
+    }else if(!validator.isEmail(data.email)){
+        errors.email = "Email is invalid";
+    }
+
+    //password checks
+    if(validator.isEmpty(data.password)){
+        errors.password = "Password field is required";
+    }
+
+    return {
+        errors,
+        isValid : isEmpty(errors)
+    };
+};
